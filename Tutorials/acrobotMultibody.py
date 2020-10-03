56e9a7f827d94c33f71b67012fab421fe89980d3
Luke Drnach <ldrnach3@gatech.edu>
2020-10-03 00:02:24 +0000

    Added acrobot tutorials

---
+++ Tutorials/acrobotMultibody.py
@@ -0,0 +1,124 @@
+"""
+acrobotMultibody.py: Tutorial script on working with MultibodyPlant in Drake, using the Acrobot as an example
+
+Luke Drnach
+October 2, 2020
+"""
+from math import pi 
+# Import utilities from pydrake
+from pydrake.common import FindResourceOrThrow
+from pydrake.all import MultibodyPlant, JacobianWrtVariable
+from pydrake.multibody.parsing import Parser
+
+# Find and load the Acrobot URDF 
+acro_file = FindResourceOrThrow("drake/examples/acrobot/Acrobot.urdf")
+# Create a Multibody plant model from the acrobot
+plant = MultibodyPlant(0.0)
+acrobot = Parser(plant).AddModelFromFile(acro_file)
+plant.Finalize()
+
+# The CONTEXT of the system stores information, such as the state, about the multibody plant. The context is necessary to calculate values such as the inertia matrix
+context = plant.CreateDefaultContext()
+
+## STATES AND DIMENSIONS
+# Evaluating and updating the position and velocity variables
+# Get and print the number of position and velocity variables
+nQ = plant.num_positions()
+nV = plant.num_velocities()
+print("The acrobot has ", nQ, " generalized positions and ", nV, " generalized velocities")
+# We can get the values of the positions and velocities from the CONTEXt
+q = plant.GetPositions(context)
+v = plant.GetVelocities(context)
+print("The acrobot default configuration is q = ", q)
+print("The acrobot default velocity is v = ", v)
+# We can also get the full state, x = [q, v]
+x = plant.GetPositionsAndVelocities(context)
+print('Acrobot full state x = ', x)
+# Drake implements a mapping between the generalized positions and velocities. The default mapping is:
+dq = plant.MapVelocityToQDot(context, v)
+print("The default acrobot configuration rates are dq = ", dq)
+
+# We can set the values for the positions and velocities as well:
+plant.SetPositionsAndVelocities(context, [pi/4, pi/4, 0.1, -0.1 ])
+# Then we can get the new values of the position and velocity
+q = plant.GetPositions(context)
+v = plant.GetVelocities(context)
+# And compare against the configuration rates
+dq = plant.MapVelocityToQDot(context, v)
+print("The modified acrobot configuration is q = ", q, " and velocity v = ", v)
+print("The acrobot configuration rates are dq =", dq)
+
+## DYNAMICS
+# We can get the mass matrix via inverse dynamics (note that CalcMassMatrix is not available in pyDrake):
+M = plant.CalcMassMatrixViaInverseDynamics(context)
+print('Acrobot mass matrix at q = ',q,' is M = ')
+print(M)
+# We can also get the combined coriolis, centripetal, and gyroscopic effects:
+Cv = plant.CalcBiasTerm(context)
+print("Acrobot bias term at q = ", q, "and v = ",v ,"is Cv = ")
+print(Cv)
+# We can separately get the gravitational effects
+N = plant.CalcGravityGeneralizedForces(context)
+print("Acrobot gravitational generalized forces at q = ", q," is N = ")
+print(N)
+# Evaluating the controls
+nU = plant.num_actuated_dofs()
+nA = plant.num_actuators()
+print('Acrobot has ', nU, ' actuated joint(s) and ', nA, ' actuator(s)')
+# We can get the actuation matrix, a permutation  matrix as:
+B = plant.MakeActuationMatrix()
+print('The acutator selection matrix for acrobot is B = ')
+print(B)
+# Note that calculating the dynamics in this fashion is not computationally efficient. It would be more efficient to use plant.CalcInverseDynamics instead, given the generalized acceleration and applied forces
+
+## KINEMATICS and GEOMETRY
+# Let's start with the frames in Acrobot
+nF = plant.num_frames()
+nJ = plant.num_joints()
+print(f"Acrobot has {nF} frames and {nJ} joints")
+nC = plant.num_collision_geometries()
+print(f"Acrobot has {nC} collision geometries")
+# We can get the world body and world frame
+wbody = plant.world_body()
+wframe = plant.world_frame()
+# We can get the bodies and the names of the bodies from the plant
+body_indices = plant.GetBodyIndices(acrobot)
+for n in range(0, len(body_indices)):
+    name = plant.get_body(body_indices[n]).name()
+    print(f"Acrobot body {n} is called {name}")
+    body_frame = plant.get_body(body_indices[n]).body_frame()
+    frame_name = body_frame.name()
+    print(f"Body {name} has a frame called {frame_name}")
+    # We can get the frame index for the body frame as
+    frame_idx = body_frame.index()
+    # Then we can calculate the location of points in the body frames in world coordinates
+    pW = plant.CalcPointsPositions(context, body_frame, [0., 0., 0.], wframe)
+    print(f"The origin of frame {frame_name} in world coordinates is")
+    print(pW)
+    # Alternatively, we can calculate the pose of the body in world coordinates
+    pose = plant.EvalBodyPoseInWorld(context, plant.get_body(body_indices[n]))
+    print(f"The pose of body {name} in world coordinates is")
+    print(pose.GetAsMatrix4())
+    # We can also calculate the Jacobian of the frame in world coordinates
+    J = plant.CalcJacobianTranslationalVelocity(context, JacobianWrtVariable.kQDot, body_frame, [0.,0.,0.], wframe, wframe)
+    print(f"The Jacobian for the origin of body {name} in world coordinates is")
+    print(J)
+
+
+# We CANNOT Get the joint indices using plant.GetJointIndices(acrobot).  The method GetJointIndices does not exist in pydrake
+ # Note - we've only explored 4 of the 8 frames in Acrobot. 
+
+# Joints and Actuators
+# Note that we can get the indices of the joints connected to actuators, provided we can get the indices/names of the actuators
+actuator = plant.GetJointActuatorByName('elbow')
+# We can check for effort limits
+print("Actuator ", actuator.name(), " has effort limit ", actuator.effort_limit())
+# We can get the joint attached to the actuator
+joint = actuator.joint()
+# Then we can query the parent body, the child body, and their frames
+joint_child = joint.frame_on_child().name()
+joint_parent = joint.frame_on_parent().name()
+joint_name = joint.name()
+print(f"Joint {joint_name} connects parent body {joint_parent} to child body {joint_child}")
+
+# Contact Geometry
---
+++ Tutorials/acrobotSwingUp.py
@@ -0,0 +1,105 @@
+"""
+acrobotSwingUp: example direct collocation trajectory optimization in pyDrake
+
+acrobotSwingUp creates and solves the trajectory optimization problem of swinging the acrobot from the downward position to the upward position. The nonlinear program is developed using the DirectTranscription class and solved using SNOPT.
+
+Luke Drnach
+September 30, 2020
+"""
+from math import pi 
+import matplotlib.pyplot as plt 
+import numpy as np 
+import timeit
+# Import utilities from pydrake
+from pydrake.common import FindResourceOrThrow
+from pydrake.all import (MultibodyPlant, PiecewisePolynomial, DirectCollocation, Solve, MathematicalProgram, SolverOptions, DiagramBuilder, SceneGraph, PlanarSceneGraphVisualizer, Simulator, TrajectorySource)
+from pydrake.multibody.parsing import Parser
+from pydrake.solvers.snopt import SnoptSolver, SnoptSolverDetails 
+
+# Find and load the Acrobot URDF 
+acro_file = FindResourceOrThrow("drake/examples/acrobot/Acrobot.urdf")
+# Create a Multibody plant model from the acrobot
+plant = MultibodyPlant(0.0)
+acrobot = Parser(plant).AddModelFromFile(acro_file)
+plant.Finalize()
+# Create the default context
+context0 = plant.CreateDefaultContext()
+# Create a direct collocation problem
+prog = DirectCollocation(
+    plant,
+    context0,
+    num_time_samples=21,
+    maximum_timestep=0.20,
+    minimum_timestep=0.05,
+    input_port_index=plant.get_actuation_input_port().get_index()
+)
+prog.AddEqualTimeIntervalsConstraints()
+# Add initial and final state constraints
+x0 = [0, 0, 0, 0]
+xf = [pi, 0, 0,0]
+prog.AddBoundingBoxConstraint(x0, x0, prog.initial_state())
+prog.AddBoundingBoxConstraint(xf, xf, prog.final_state())
+# Add running cost
+u = prog.input()
+R = 10
+prog.AddRunningCost(R * u[0] ** 2)
+# Add final cost
+prog.AddFinalCost(prog.time())
+# Create an initial guess at the trajectory
+init_x = PiecewisePolynomial.FirstOrderHold([0, 10.0], np.column_stack((x0, xf)))
+init_u = PiecewisePolynomial.FirstOrderHold([0, 10.0], np.zeros(shape=(1,2)))
+prog.SetInitialTrajectory(traj_init_u=init_u, traj_init_x=init_x)
+# Set SNOPT options
+prog.SetSolverOption(SnoptSolver().solver_id(), "Major Iterations Limit", 1000)
+prog.SetSolverOption(SnoptSolver().solver_id(), "Major Feasibility Tolerance", 1e-4)
+prog.SetSolverOption(SnoptSolver().solver_id(), "Major Optimality Tolerance", 1e-4)
+prog.SetSolverOption(SnoptSolver().solver_id(), "Scale Option", 2)
+solver = SnoptSolver()
+# Solve the problem
+print("Solving trajectory optimization")
+start = timeit.default_timer()
+result = solver.Solve(prog)
+stop = timeit.default_timer()
+print("Elapsed Time: ", stop-start)
+# Get the details  of the solution
+print("Optimization successful? ", result.is_success())
+print('solver is: ', result.get_solver_id().name())
+print('optimal cost = ', result.get_optimal_cost())
+# Get the exit code from SNOPT
+details = result.get_solver_details()
+print('SNOPT Exit Status: ',details.info)
+
+# Unpack the trajectories
+u_traj = prog.ReconstructInputTrajectory(result)
+x_traj = prog.ReconstructStateTrajectory(result)
+time = np.linspace(u_traj.start_time(), u_traj.end_time(), 101)
+u_lookup = np.vectorize(u_traj.value)
+u = u_lookup(time)
+x = np.hstack([x_traj.value(t) for t in time])
+
+# Plot the trajectory
+plt.figure()
+plt.subplot(3,1,1)
+plt.plot(time, x[0,:],label="shoulder")
+plt.plot(time, x[1,:],label="elbow")
+plt.legend()
+plt.ylabel('Positions (rad)')
+plt.subplot(3,1,2)
+plt.plot(time, x[2,:])
+plt.plot(time, x[3,:])
+plt.ylabel('Velocities (rad/s)')
+plt.subplot(3,1,3)
+plt.plot(time, u)
+plt.ylabel('Torque (Nm)')
+plt.xlabel('Time (s)')
+plt.show()
+
+# Drake Visualization
+# builder = DiagramBuilder()
+# source = builder.AddSystem(TrajectorySource(x_traj))
+# scene_graph = builder.AddSystem(SceneGraph())
+
+# visualizer = builder.AddSystem(PlanarSceneGraphVisualizer(scene_graph, xlim=[-4.,4.], ylim=[-4., 4.], show=plt_is_iteractive))
+
+
+print('success')
